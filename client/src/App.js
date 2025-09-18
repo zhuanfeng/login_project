f50a29b21cea7623bec73197f94cf8f31dda1c42
liu <liufpszu@gmail.com>
2025-09-18 23:50:24 +0800

    inital client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>用户登记系统</h1>
+        <p>系统正在初始化...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,11 @@
+这是一个用户登录程序，以下是如何配置环境：
+
+1.配置python的环境：pip install -r server/requirements.txt
+
+2.配置react等的环境：
+
+    1).下载node.js（官网https://nodejs.org/），并将其添加到path
+
+    1).npm install  （仅第一次运行需要)
+
+    2).npm start
